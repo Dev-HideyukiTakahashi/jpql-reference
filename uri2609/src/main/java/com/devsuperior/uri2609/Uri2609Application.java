016f34917f36bc18d14ea3dfe97908754730bdd5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-26 19:23:22 -0300

    uri 2609

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,10 +1,13 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -12,13 +15,15 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2609Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		List<CategorySumDTO> list = repository.amountGroupedByCategory();
+		System.out.println("----------RESULTADO----------");
+		list.forEach(obj -> System.out.println("Category Name: " + obj.getName() + ", Sum: " + obj.getSum()));
 	}
 }
--- uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,9 +1,30 @@
 package com.devsuperior.uri2609.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+  /*
+   * SQL:
+   * 
+   * SELECT categories.name, SUM(products.amount)
+   * FROM products
+   * INNER JOIN categories
+   * ON products.id_categories = categories.id
+   * GROUP BY categories.name;
+   * 
+   */
+
+  // JPQL:
+
+  @Query("SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) "
+      + "FROM Product obj "
+      + "GROUP BY obj.category.name")
+  List<CategorySumDTO> amountGroupedByCategory();
 }
