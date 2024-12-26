2a03a337c8122f4bf19fc27e438dca0630f10a31
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-26 20:29:48 -0300

    uri 2737

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,10 +1,14 @@
 package com.devsuperior.uri2737;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
 @SpringBootApplication
@@ -12,14 +16,16 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Autowired
 	private LawyerRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2737Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-
+		List<LawyerMinProjection> result = repository.search1();
+		List<LawyerMinDTO> resultDTO = result.stream().map(x -> new LawyerMinDTO(x)).toList();
+		System.out.println("----------RESULTADO----------");
+		resultDTO.forEach(x -> System.out.println(x));
 	}
 }
